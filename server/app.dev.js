3b8f9714400a2a5b1d243ab8b7692c5384485406
html5@mobile <phpython8@gmail.com>
2018-10-22 07:55:52 +0800

    add compiler

--- server/app.dev.js
+++ server/app.dev.js
@@ -3,23 +3,12 @@ const path = require('path');
 const webpack = require('webpack');
 const webpackConfig = require('../config/webpack.config.dev.js');
 const compiler = webpack(webpackConfig);
+const { getAssetManifest } = require('./utils');
 const webpackDevMiddleware = require('webpack-dev-middleware')(compiler, {
 	serverSideRender: true,
-	noInfo: true, 
+	noInfo: true,
 	publicPath: webpackConfig.output.publicPath
 });
-console.log('output::::', webpackConfig.output.publicPath);
-const isObject = require('is-object');
-function getAssetManifest(res) {
-	const content = res.locals.fs.readFileSync(__dirname + '/../build/asset-manifest.json');
-	return JSON.parse(content);
-}
-function normalizeAssets(assets) {
-	if (assets) {
-		return Object.values(assets);
-	}
-	return Array.isArray(assets) ? assets : [ assets ];
-}
 const app = express();
 
 let assetManifest = null;
@@ -40,47 +29,15 @@ app.use('/api/count', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-	// console.log('output', res.locals.webpackStats.toJson().assetsByChunkName);
-	// if (!assetManifest) {
-	// 	assetManifest = getAssetManifest(res);
-	// }
-	const assetsByChunkName = res.locals.webpackStats.toJson().assetsByChunkName;
-	const fs = res.locals.fs;
-	console.log(assetsByChunkName);
-	const outputPath = res.locals.webpackStats.toJson().outputPath;
-	// return res.render('index', {
-	// 	title: 'ssr',
-	// 	PUBLIC_URL: '',
-	// 	assetManifest: 
-	// });
-	return res.send(`
-<html>
-  <head>
-    <title>ssr</title>
-    <style>
-		${normalizeAssets(assetsByChunkName.main)
-			.filter((path) => path.endsWith('.css'))
-			.map((path) => fs.readFileSync(outputPath + '/' + path))
-			.join('\n')}
-    </style>
-  </head>
-  <body>
-	<div id="root"></div>
-	${normalizeAssets(assetsByChunkName['runtime~main'])
-		.filter((path) => path.endsWith('.js'))
-		.map((path) => `<script src="${path}"></script>`)
-		.join('\n')}
-	${normalizeAssets(assetsByChunkName['vendors'])
-		.filter((path) => path.endsWith('.js'))
-		.map((path) => `<script src="${path}"></script>`)
-		.join('\n')}
-		${normalizeAssets(assetsByChunkName.main)
-			.filter((path) => path.endsWith('.js'))
-			.map((path) => `<script src="${path}"></script>`)
-			.join('\n')}
-  </body>
-</html>
-  `);
+	if (!assetManifest) {
+		assetManifest = getAssetManifest(res);
+	}
+	return res.render('index', {
+		title: 'ssr-express',
+		PUBLIC_URL: '',
+		isProd: false,
+		assetManifest,
+	});
 });
 
 app.set('view engine', 'ejs');
